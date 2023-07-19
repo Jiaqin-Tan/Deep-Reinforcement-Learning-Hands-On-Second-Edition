c349770a93c0ab3c2e654712e69be179ffc50994
jia <zakorph@gmail.com>
2023-07-19 23:15:49 +1000

    comit4

--- Chapter02/03_random_action_wrapper.py
+++ Chapter02/03_random_action_wrapper.py
@@ -16,18 +16,26 @@ class RandomActionWrapper(gym.ActionWrapper):
         # as you know, if you don't keep doing one thing, thing you will forgot. 
         # inside super() is call argument/parameter
         # super() can take two, first is the subclass, second is the object of that subclass
-            # 
+            # the two parameter seems not necessary in this case, could be this code is to work with python2
         self.epsilon = epsilon
 
+    # override parent's agent action
     def action(self, action: Action) -> Action:
         if random.random() < self.epsilon:
             print("Random!")
             return self.env.action_space.sample()
+            # this returns an action, so it replaces the action obj passed in in the parameter
+            # and then the action is returned below
         return action
 
 
 if __name__ == "__main__":
     env = RandomActionWrapper(gym.make("CartPole-v1"))
+    # our Wrapper becomes the Env
+    # what we modified is the action space. to just one and force the agent to take it.
+    
+    # below code is the same as the code in 02_cartpole_random.py
+    # meaning all you need to do is modify the Env. 
 
     obs = env.reset()
     total_reward = 0.0
