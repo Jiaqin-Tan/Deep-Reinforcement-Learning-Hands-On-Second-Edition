b20527e78166bd514563b5e23f8ecdb7d95c4714
jia <zakorph@gmail.com>
2023-07-19 23:30:29 +1000

    comit5

--- Chapter02/04_cartpole_random_monitor.py
+++ Chapter02/04_cartpole_random_monitor.py
@@ -4,6 +4,7 @@ import gym
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
     env = gym.wrappers.Monitor(env, "recording")
+    # seems like Monitor module is removed. and replaced by other module, maybe. 
 
     total_reward = 0.0
     total_steps = 0
@@ -11,7 +12,7 @@ if __name__ == "__main__":
 
     while True:
         action = env.action_space.sample()
-        obs, reward, done, _ = env.step(action)
+        obs, reward, done, _, xx = env.step(action)
         total_reward += reward
         total_steps += 1
         if done:
