80f50998fa6f9b51983e4402c163174a982d1881
jia <zakorph@gmail.com>
2023-07-19 18:49:52 +1000

    comit2

--- Chapter02/02_cartpole_random.py
+++ Chapter02/02_cartpole_random.py
@@ -2,19 +2,30 @@ import gym
 
 
 if __name__ == "__main__":
-    env = gym.make("CartPole-v0")
+    env = gym.make("CartPole-v1") # Create the environment
 
-    total_reward = 0.0
+    total_reward = 0.0  
     total_steps = 0
-    obs = env.reset()
+    obs = env.reset()   # Reset the environment before starting
+    ddd3 = env.action_space
+    ddd4 = env.observation_space
+    
 
     while True:
-        action = env.action_space.sample()
-        obs, reward, done, _ = env.step(action)
+        action = env.action_space.sample()  # Sample random action
+        obs, reward, done, _, x = env.step(action)  # Take action and store the outcomes
+        # the code got updated now it return 5 items
         total_reward += reward
         total_steps += 1
+        
         if done:
             break
 
     print("Episode done in %d steps, total reward %.2f" % (
         total_steps, total_reward))
+    print(ddd3, ddd4)
+
+
+
+
+
--- Chapter02/03_random_action_wrapper.py
+++ Chapter02/03_random_action_wrapper.py
@@ -8,6 +8,12 @@ Action = TypeVar('Action')
 class RandomActionWrapper(gym.ActionWrapper):
     def __init__(self, env, epsilon=0.1):
         super(RandomActionWrapper, self).__init__(env)
+        # super() function: give access to the methods of a parent class. returns a temporary object of a parent class when used.
+        # super().__init__(env) is equivalent to gym.ActionWrapper.__init__(env)
+        #__init__ is the "def __init__" method... you are just not good at coding... these concepts...
+        # def = creating method
+        # as you know, if you don't keep doing one thing, thing you will forgot. 
+        
         self.epsilon = epsilon
 
     def action(self, action: Action) -> Action:
@@ -18,13 +24,13 @@ class RandomActionWrapper(gym.ActionWrapper):
 
 
 if __name__ == "__main__":
-    env = RandomActionWrapper(gym.make("CartPole-v0"))
+    env = RandomActionWrapper(gym.make("CartPole-v1"))
 
     obs = env.reset()
     total_reward = 0.0
 
     while True:
-        obs, reward, done, _ = env.step(0)
+        obs, reward, done, _, xz = env.step(0)
         total_reward += reward
         if done:
             break
